3888ab0761133eaf639c78af1566c721556f86d2
abdmun8@gmail.com <abdmun8@gmail.com>
2020-09-01 09:36:19 +0700

    update

--- index.js
+++ index.js
@@ -3,26 +3,29 @@ const fs = require("fs");
 
 // config
 const cookies =
-  "_ga=GA1.3.92820565.1596426578; SPUsageId=1ae08f04-3f64-4566-a4f2-31e20bb13913; _gid=GA1.3.444622237.1598457632; FedAuth=77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+MCMuZnxsZGFwbWVtYmVyfHRvZHN1cHBvcnQubXN0LDAjLmZ8bGRhcG1lbWJlcnx0b2RzdXBwb3J0Lm1zdCwxMzI0MzM2MzI5MjEyNDI0MDYsVHJ1ZSxqOHpLSmpTQVlEeWI3blladEhhN2RJUk5qU3hKcXhuUEpFc2NkOStDQ2RUd2g3dUhkTEJVT3hRWENHSDJhZmFHNEZXVnpnYnA1ckhJd0xZUUxta3pSZGhWa0dwaVBhaDlabjZvR3dkUUVlTFBLNmk1ZStxQWtGN0orVUlTOGtRT3lZNy9nSkpyVkE4eEFOZUNmN3B1MG1WSjVMOVJKMGRML3ZWdGhPZWRHd0h1eFJpY1BFUXFtVVpZSjkxRGdiajYxS1BtOUhoclRrbDF4cDR4K1VhUWtuNjlZcFVLbXJIZjZWRHlvNmw0bHh4T1d1eGUrTG05OWJyeFBGVWs3cjY5OW1CMWZ2b203T1lwWUtzelVwWXA0OWZkQU0yNmlEeGpHbzBiUWZMam0rUkpXSjM3T21TUWw3c2ZZZUxKQnMzeXNnM2cyR1l3Y2lmSExXTHljRHhzNXc9PSxodHRwOi8vcG9ydGFsLnRyYWtpbmRvLmNvLmlkL19sYXlvdXRzLzE1L1RyYWtpbmRvL0F1dGhlbnRpY2F0aW9uL0xvZ2luLmFzcHg/UmV0dXJuVXJsPS9fbGF5b3V0cy8xNS9BdXRoZW50aWNhdGUuYXNweD9Tb3VyY2U9JTI1MkYmYW1wO1NvdXJjZT0vPC9TUD4=; sp=QbHXY8f9lL/3spy9DIS+7A==; spPass=NuZQzlnOnlLbxGj2WP2CGw==; spId=2957292; .AspNetCore.Session=CfDJ8C1k0eKKxIxOrFiNmveedC75kNPfAkoGsq7Ra72C29hvvq50fU4yRDdI6V9K4yfKXFdo66ZxdtWP7BlD1xGWDcl%2BUc78eGn5sUjxfwNud4pJ4IxPvlss1HDKitXan34A4XCWSBv8oArKGTgriu9HMq%2BDrWuoXguH7K2NqYVJwhos; _gat=1";
-const url = "https://tod-api.trakindo.co.id/api/service/quotation";
-const api = { endpoint: url, method: "GET", data: {} };
+  "_ga=GA1.3.1508858433.1596425083; SPUsageId=b9e00243-61c2-48ef-a557-9428d8436a3b; _gid=GA1.3.1420588977.1598839849; FedAuth=77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+MCMuZnxsZGFwbWVtYmVyfHRvZHN1cHBvcnQubXN0LDAjLmZ8bGRhcG1lbWJlcnx0b2RzdXBwb3J0Lm1zdCwxMzI0MzgyMTQyMjg3NjE0MTMsVHJ1ZSxKM2dCN3VxTGM2T2dVVTJxU0N1dmtmZjNwWTdYMDFuRVFxOVUrWkV3QzhVcmhhTDhRQ2lEclZ0RUtQWEtvZXlwZUxvWlZmcG9tbyt0SXcrcFFmc0lEMUE3R1NyK3JXV28wWXhhR3k1dUxjWEw3SksvcWlpS0Rsa1JwRis3dUZreXdYOWtIWUhiVENZdU4rcmZwdTc5UE5zWVdiQnR2MHQ4aDJuaEc2UFRhdk5oMlRHZjlJejB4UFJZa0hGZzJUcEdrL0hlclJWWlAzaWlzRzFNVEVnL2h6YzR4SFdaVzFsWEtpNWZiYVQxRmU2U0dPYnJ6S2diRzZNS3ZpRE5vTndkaVQyNzhtTm1SVjIvdHltdzYrM3hFeXRsWGU4NWNMSW9uMFhEYityWHJoTG9TcnFHRDFjLzRJQ05iV3dwK1ZzSUs0d1BSNlpxQmgzQlFGZDEvN21mV0E9PSxodHRwOi8vcG9ydGFsLnRyYWtpbmRvLmNvLmlkL19sYXlvdXRzLzE1L1RyYWtpbmRvL0F1dGhlbnRpY2F0aW9uL0xvZ2luLmFzcHg/bG9nb3V0PTEmYW1wO3JlYXNvbj0zPC9TUD4=; sp=QbHXY8f9lL/3spy9DIS+7A==; spPass=NuZQzlnOnlLbxGj2WP2CGw==; spId=2975576; _gat=1; .AspNetCore.Session=CfDJ8F0jlBWYXpRBq1Hum668BlQISvmVcjjcv2uCjyyluX1wAeeOhya7nETOGvMXyvN6kgR2NyRzMY9cZ8SxT1FbMoTUgCF2ey3OFQqLix8wn%2FFMDb3eQf6AROXIZ46K%2BgV1Yn05Ys5Mp06Ez6vXox3Mu6o6ixpxlsx8uTULDb1dnv8a";
+const host = "https://tod-api.trakindo.co.id/api/service/";
+const urls = [
+  "serviceRequest",
+  "quotation",
+  "readyToExecute",
+  "serviceRequestWip",
+  "jobComplete",
+];
 const interval = 300000; //ms 5m
-const timeout = 30;
+const timeout = 120;
 const token = "";
 
-// init
-writeFile(true);
-
 setInterval(() => {
-  const req = request(api, { page: 1, areaCode: "ALL" }, null);
-  const dt = new Date();
-  const reqStart = dt.toLocaleString();
-  req
-    .then(() => console.log(`Success: ${new Date().toLocaleString()}`))
-    .catch((e) => {
-      writeFile(false, reqStart);
-    });
-}, interval);
+  urls.forEach((urlItem) => {
+    const api = { endpoint: urlItem, method: "GET", data: {} };
+    const req = request(api, { page: 1, areaCode: "ALL" });
+    const reqStart = new Date();
+    req
+      .then(() => writeFile(urlItem, reqStart)) //console.log(`Success: ${new Date().toLocaleString()}`))
+      .catch((e) => writeFile(urlItem, reqStart, "Failed"));
+  }, interval);
+});
 
 /**
  * write log to file
@@ -30,15 +33,57 @@ setInterval(() => {
  * @param {*} reqStart
  */
 // write file
-function writeFile(begin = false, reqStart = null) {
+function writeFile(url, reqStart, status = "Success") {
+  let file = "";
+  switch (url) {
+    case "quotation":
+      file =
+        status == "Success" ? "quotation/success.log" : "quotation/error.log";
+      break;
+    case "readyToExecute":
+      file =
+        status == "Success"
+          ? "readyToExecute/success.log"
+          : "readyToExecute/error.log";
+      break;
+    case "serviceRequestWip":
+      file =
+        status == "Success"
+          ? "serviceRequestWip/success.log"
+          : "serviceRequestWip/error.log";
+      break;
+    case "jobComplete":
+      file =
+        status == "Success"
+          ? "jobComplete/success.log"
+          : "jobComplete/error.log";
+      break;
+    default:
+      file =
+        status == "Success"
+          ? "serviceRequest/success.log"
+          : "serviceRequest/error.log";
+      break;
+  }
+
+  const reqComplete = new Date();
+  const duration = parseInt(reqComplete.getTime() - reqStart.getTime());
   fs.appendFile(
-    "./error.txt",
-    begin
-      ? `LOG every ${interval / 60000} Minutes -- start\n`
-      : `Error: start-> ${reqStart} end-> ${new Date().toLocaleString()}\n`,
+    file,
+    `${status} [start]: ${reqStart.toString()} [duration]: ${convertDuration(
+      duration
+    )}\n`,
     "UTF-8",
-    () => console.log(`Error: ${new Date().toLocaleString()}`)
+    () => console.log(`writed: ${new Date().toString()}`)
   );
+}
+
+function convertDuration(time) {
+  if (time < 1000) {
+    return `${time} ms`;
+  } else {
+    return time / 1000 + " s";
+  }
 }
 
 /**
@@ -74,14 +119,15 @@ function request(api, param) {
     axiosConfig.data = params;
   }
 
+  let reqStart = new Date();
   return new Promise((resolve, reject) => {
     axios(axiosConfig)
       .then((response) => {
         const body = response.data;
-        resolve(body);
+        resolve(reqStart);
       })
       .catch((err) => {
-        reject(err);
+        reject(reqStart);
       });
   });
 }
